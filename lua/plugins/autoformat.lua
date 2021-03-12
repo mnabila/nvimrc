d3cc9a7578266829b4d1dba392b337db9ca04421
M. Nabil Adani <nblid48@gmail.com>
2021-03-12 16:33:19 +0700

    chore: delete unuse configurations
    list deleted configurations:
    - lua/modules/libs/buffer.lua
    - lua/modules/statusline/function.lua
    - lua/modules/statusline/init.lua
    - lua/plugins/autoformat.lua
    - lua/plugins/coc.lua
    - lua/plugins/fzf.lua
    - lua/plugins/nvimBufferLine.lua
    - lua/plugins/signify.lua
    - lua/plugins/tagbar.lua

--- lua/plugins/autoformat.lua
+++
@@ -1,7 +0,0 @@
--- bash
-vim.g.formatdef_shfmt = '"shfmt -i ".(&expandtab ? shiftwidth() : "4")'
-vim.g.formatters_sh = {'shfmt'}
-
--- lua
-vim.g.formatdef_luaformatter = '"lua-format"'
-vim.g.formatters_lua = {'luaformatter'}
--- lua/plugins/fzf.lua
+++
@@ -1,5 +0,0 @@
-vim.g.fzf_layout = {window = {width = 0.9, height = 0.8, highlight = 'Normal', rounded = false}}
-vim.cmd([[
-autocmd! FileType fzf tnoremap <buffer> <esc> <c-c>
-command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, fzf#vim#with_preview({"options": ["--info=inline"]}), <bang>0)
-]])
